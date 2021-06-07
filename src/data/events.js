881f9d857f3c40d2d2162a3d78e4119fb80f3c8a
Jamie Humphries <jamie.humphries@softwire.com>
2021-06-07 22:33:27 +0100

    Automatically tag events.

--- src/data/events.js
+++ src/data/events.js
@@ -1,4 +1,4 @@
-module.exports = [
+const events = [
   {
     episode: 1,
     scene: 1,
@@ -7,7 +7,24 @@ module.exports = [
     description: [
       "[Jerry](@jerry-wilkinson) and [Deb](@deborah) pull a [cracker](#christmas-crackers) on Zoom.",
     ],
-    characters: ["deborah", "jerry-wilkinson"],
-    themes: ["christmas-crackers"],
   },
 ];
+
+module.exports = events.map((event) => {
+  event.characters = event.characters || [];
+  event.themes = event.themes || [];
+  const matches = event.description
+    .join(" ")
+    .matchAll(/\[[^\]]+\]\(([@#][a-z-]+)\)/g);
+  for (const match of matches) {
+    const tag = match[1];
+    const type = tag[0];
+    const id = tag.substring(1);
+    if (type === "@") {
+      event.characters.push(id);
+    } else if (type === "#") {
+      event.themes.push(id);
+    }
+  }
+  return event;
+});
